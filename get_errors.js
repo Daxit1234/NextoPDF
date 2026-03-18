ea439f7da4c26bc48e1dad9e5de682a9df185bfc
Daxit Godhani <dakshitgodhani103@gmail.com>
2026-03-18 20:27:10 +0530

    remove unused files

--- get_errors.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-try {
-  execSync('npx tsc --noEmit', { stdio: 'pipe' });
-  fs.writeFileSync('ts-errors-clean.txt', 'Success!');
-} catch (e) {
-  fs.writeFileSync('ts-errors-clean.txt', (e.stdout ? e.stdout.toString() : '') + (e.stderr ? e.stderr.toString() : ''));
-}
